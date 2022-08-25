81c5ef5732ef7c05c510f42c76287547907af59d
jem-thanmay <jembige.thanmay@gmail.com>
2022-08-25 12:57:25 +0530

    Add project files.

---
+++ Calsio.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConsoleApp99
+{
+
+
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static void Main()
+        { }
+    }
+}
